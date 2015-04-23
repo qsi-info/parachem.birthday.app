d04cce55b410c3a85448a4b1e8b6f56bf2948044
Alexandre Page <alexgroove24@gmail.com>
2015-04-23 13:11:41 -0400

    add external librairy

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,83 +9,12 @@
  * Main module of the application.
  */
 
-
-function parseQueryString() {
-  var query = (window.location.search || '?').substr(1);
-  var map = {};
-  query.replace(/([^&=]+)=?([^&]*)(?:&+|$)/g, function (match, key, value) {
-    (map[key] = map[key] || []).push(window.decodeURIComponent(value));
-  });
-  return map;
-}
+ /*jshint unused:false*/
+ /*jshint asi:true*/
 
 
 
-angular
-  .module('AngularSharePointApp', [
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'cfp.loadingBar',
-  ])
-
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-
-      // Home
-      .when('/home', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-
-      // Setup
-      .when('/gateway', {
-        template: '',
-        controller: 'GatewayCtrl',
-      })
-
-      .when('/reload', {
-        templateUrl: 'views/reload.html',
-      })      
-
-      // Default
-      .otherwise({
-        redirectTo: '/gateway'
-      });
-
-
-  }])
-
-
-  .run(['$location', '$rootScope', function ($location, $rootScope) {
-
-    var host, app, params, sender, isWebPart = true;
-
-    try {
-      params = parseQueryString();
-      host = params.SPHostUrl[0];
-      app = params.SPAppWebUrl[0];
-      sender = params.SenderId[0];
-    } catch(e) {
-      params = $location.search();
-      host = params.SPHostUrl;
-      app = params.SPAppWebUrl;
-      sender = params.SenderId;
-      isWebPart = false;
-    }
-
-    $rootScope.sp = {
-      host: host,
-      app: app, 
-      sender: sender,
-      isWebPart: isWebPart,
-    };
-
-    $rootScope.isInitialize = false;
-
-
-  }]);
-
+angular.module('AngularSharePointApp', ['ngSharePoint', 'ngRoute']);
 
 
 
