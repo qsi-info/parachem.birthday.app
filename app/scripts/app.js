cbfcb7611054e0570000999f49bdde02a95c377a
Alexandre Page <alexgroove24@gmail.com>
2015-04-29 12:33:28 -0400

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,10 +11,56 @@
 
  /*jshint unused:false*/
  /*jshint asi:true*/
+ /*global moment:true*/
 
 
 
-angular.module('AngularSharePointApp', ['ngSharePoint', 'ngRoute']);
+angular.module('AngularSharePointApp', ['ngSharePoint', 'ngRoute'])
+
+
+.config(['$routeProvider', function ($routeProvider) {
+
+	$routeProvider.when('/', {
+		controller: 'MainController',
+		templateUrl: 'views/main.html',
+	})
+
+}])
+
+
+.factory('Anniversaires', ['SharePoint', function (SharePoint) {
+
+	return new SharePoint.API.List('Anniversaires');
+
+}])
+
+
+.controller('MainController', ['Anniversaires', '$scope', '$rootScope', function (Anniversaires, $scope, $rootScope) {
+
+	Anniversaires.all('$select=Anniversaire,Personne/Title,Personne/Id&$expand=Personne').then(function (acc) {
+		show_birthdays(acc);
+	});
+
+	var today = moment();
+	$scope.people = [];
+	$scope.myBirthday = false
+
+	var show_birthdays = function (birthdays) {
+		birthdays.forEach(function (birthday) {
+			if (moment(birthday.Anniversaire).isSame(today, 'day')) {
+				$scope.people.push(birthday.Personne);
+			}
+		});
+
+		for (var i = 0; i < $scope.people.length; i++) {
+			if ($rootScope.me.get_id() == $scope.people[i].Id) {
+				$scope.myBirthday = true;
+			}
+		}
+	};
+
+}]);
+
 
 
 
